55dccc509b38cffe6be83797fd56706c9c32d308
Joshua <joshuaprpich@gmail.com>
2019-03-25 19:42:28 -0400

    add two sum

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+# write a function that returns the indices of array elemts that sum to 0
+def two_sum(arr1, arr2):
+  result = []
+  for i in range(len(arr1)): 
+    for j in range(len(arr2)):
+      if arr1[i] + arr2[j] == 0:
+        result.append([i,j]) 
+  return result 
+
+print(two_sum([1,2,3], [-1,2,3]))
